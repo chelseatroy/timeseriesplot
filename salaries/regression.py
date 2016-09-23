b15fa930c21ef1a2987273da8e3b5c26ea28673e
chelseatroy <chelsea.dommert@gmail.com>
2016-09-23 07:53:55 -0500

    normalize continuous variables and switch from linear model to Lasso

--- salaries/regression.py
+++ salaries/regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import datasets, preprocessing, linear_model
 
 def predict_salaries(employee_dataframe):
     print "dataframe: "
@@ -8,35 +8,34 @@ def predict_salaries(employee_dataframe):
 
     df = employee_dataframe.dropna()
 
+    df['age_std'] = preprocessing.scale(df['age'])
+    df['years_experience_std'] = preprocessing.scale(df['years_experience'])
+
     ethnicities = df['ethnicity'].unique()
-    print(ethnicities)
     df['ethnicities_num'] = map(lambda x: np.where(ethnicities==x)[0][0], df['ethnicity'])
 
     genders = df['gender'].unique()
-    print(genders)
     df['genders_num'] = map(lambda x: np.where(genders == x)[0][0], df['gender'])
 
     roles = df['role'].unique()
-    print(roles)
     df['roles_num'] = map(lambda x: np.where(roles == x)[0][0], df['role'])
 
-    results = df['salary'].values
+    train_y = df['salary'].values
 
     df = df.drop(['ethnicity', 'gender', 'role', 'salary'], axis=1)
+    df = df[['ethnicities_num', 'genders_num', 'roles_num', 'age_std', 'years_experience_std']]
 
     print df
 
-    train_data = df.values
+    train_x = df.values
 
-    print(train_data)
-
-    trainer = linear_model.LinearRegression()
-    trainer.fit(train_data, results)
+    trainer = linear_model.Lasso(alpha = 0.1)
+    trainer.fit(train_x, train_y)
 
     print('Coefficients: \n', trainer.coef_)
 
     print("Residual sum of squares: %.2f"
-          % np.mean((trainer.predict(train_data) - results) ** 2))
+          % np.mean((trainer.predict(train_x) - train_y) ** 2))
 
     # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.2f' % trainer.score(train_data, results))
+    print('Variance score: %.2f' % trainer.score(train_x, train_y))
