0e85861553802a1544ec15eb9639e42494703da0
chelseatroy <chelsea.dommert@gmail.com>
2016-09-27 21:37:13 -0500

    Create matplotlib graphic in the app

--- data/urls.py
+++ data/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.data, name='data'),
     url(r'^data/$', views.enter_data, name='enter_data'),
     url(r'^results/$', views.display_results, name='display_results'),
+    url(r'^testplot/$', views.test_matplotlib, name='test_matplotlib'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from .regression import predict
 from .forms import DataForm
 import pandas as pd
+import matplotlib
+from pylab import figure, axes, pie, title
+from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 class DataKeeper:
     results = "DERP"
@@ -25,3 +28,18 @@ def enter_data(request):
 def display_results(request):
     results = DataKeeper.results
     return render(request, 'timeseries/results.html', {'results': results})
+
+def test_matplotlib(request):
+    f = figure(figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    fracs = [15,30,45, 10]
+    explode=(0, 0.05, 0, 0)
+    pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
+    title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
+    matplotlib.pyplot.close(f)
+
+    canvas = FigureCanvasAgg(f)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
